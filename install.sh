55777b863ebce9755081262681a398fb7e47b6ad
Aqeel AlShafei <4000465+aqshafei@users.noreply.github.com>
2024-09-02 12:19:27 +0100

    Remove spell folder copy from install script as it is not used yet

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 cp "./init.lua" "$NVIM_DIR/"
 cp -r "./lua" "$NVIM_DIR/"
 cp -r "./plugin" "$NVIM_DIR/"
-cp -r "./spell" "$NVIM_DIR"
+# cp -r "./spell" "$NVIM_DIR"
 
 echo "Installation complete."
 exit 0
